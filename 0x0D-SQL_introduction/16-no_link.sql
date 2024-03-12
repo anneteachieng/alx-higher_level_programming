0c380d061da438a70b69c06d62005ceaca2f695c
anneteachieng <achiengomondi02@gmail.com>
2024-03-12 12:07:43 -0700

    advancedsql

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+-- lists all records of the table second_table.
+-- Results should display the score and the name (in this order)
+-- Records should be listed by descending score
+
+SELECT score, name FROM second_table WHERE name IS NOT NULL AND name != '' ORDER BY score DESC;
